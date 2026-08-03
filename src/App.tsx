7f67aee56c7c4346dfbadbbf11bf34a387d75d0a
rubengitdev <rubencahyadi504@gmail.com>
2026-08-04 02:50:51 +0700

    Refactor: Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 
-function PetGame() {
-    return <></>;
-}
+const PetGame = () => {
+    return (
+        <>
+            <header>
+                <h1>Digital Pet Game</h1>
+                <p>Take care of your virtual companion!</p>
+            </header>
+        </>
+    );
+};
 
 export default PetGame;
