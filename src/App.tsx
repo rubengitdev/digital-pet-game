f0485f715a19147e074c6bb53e4b7414e673f0d1
rubengitdev <rubencahyadi504@gmail.com>
2026-08-04 03:02:52 +0700

    Refactor: Add 1st view render

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,21 @@ import './App.css';
 const PetGame = () => {
     return (
         <>
-            <header>
-                <h1>Digital Pet Game</h1>
-                <p>Take care of your virtual companion!</p>
-            </header>
+            <main>
+                <header>
+                    <h1>Digital Pet Game</h1>
+                    <p>Take care of your virtual companion!</p>
+                </header>
+                <section className="base-container info-panel">
+                    <form className="start-questions">
+                        <label htmlFor="pet-name">
+                            What is your pet's name?
+                        </label>
+                        <input type="text" id="pet-name" />
+                        <button id="set-name-btn">Start Game</button>
+                    </form>
+                </section>
+            </main>
         </>
     );
 };
